89466574e029ee068b8ab48cbd70f4deba10e5bb
MikolajRusin <rusinmiko@gmail.com>
2025-10-29 19:55:59 +0100

    feat: loading dataloader function has been implemented

---
+++ utils/data/load_dataloader.py
@@ -0,0 +1,34 @@
+from .load_dataset import LoadDataset
+from torch.utils.data import DataLoader
+from pathlib import Path
+import albumentations as A
+from typing import Any
+import torch
+
+def collate_fn(batch) -> tuple[list[torch.Tensor], list[dict[str, torch.Tensor]]]:
+    images, targets = zip(*batch)
+    images = list(images)
+    targets = [{k: v for k, v in t.items()} for t in targets]
+    return images, targets
+
+def load_dataloader(root_dir: Path | str, coco_path: Path | str, set_ratio: float | int | None = None, custom_categories: dict[str, Any] | None = None, 
+                    batch_size: int = 1, shuffle: bool = False, transform_func: A.Compose | None = None, normalize_boxes: bool = False, pin_memory: bool = False):
+    dataset = LoadDataset(
+        dataset_dir_path=Path(root_dir),
+        coco_annotations_path=Path(coco_path),
+        set_ratio=set_ratio,
+        transforms=transform_func,
+        normalize_boxes=normalize_boxes
+    )
+    if custom_categories is not None:
+        dataset.load_categories(custom_categories=custom_categories)
+
+    dataloader = DataLoader(
+        dataset=dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        collate_fn=collate_fn,
+        pin_memory=pin_memory
+    )
+
+    return dataloader
