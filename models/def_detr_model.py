0153c15e2e21b8393dc88a3054957212689c845a
MikolajRusin <rusinmiko@gmail.com>
2025-10-29 19:58:11 +0100

    feat: created class do deformable detr model

--- models/def_detr_model.py
+++ models/def_detr_model.py
@@ -1,18 +1,18 @@
 from transformers import AutoImageProcessor, DeformableDetrConfig, DeformableDetrObjectDetection
-from dataclasses import dataclass
 import torch
 import torch.nn as nn
 
-@dataclass
-class DefDetrModel(nn.module):
-    model_id: str
-    id2label: dict[int, str] | None = None
-    device: str = 'cpu'
-
-    def __post_init__(self):
+class DefDetrModel(nn.Module):
+    def __init__(self, model_id: str, id2label: dict[int, str] | None = None, device: str | None = None):
         super().__init__()
+        self.device = device or ('cuda' if torch.cuda.is_available() else 'cpu')
+        self.model_id = model_id
+        self.id2label = id2label
+
+        # --- load config and model
         self.config = self._load_model_config()
-        self.model  = self._load_model()
+        self.model = self._load_model().to(device)
+        self.processor = self.AutoImageProcessor.from_pretrained(self.model_id, use_fast=True)
 
     def _load_model_config(self):
         config = DeformableDetrConfig.from_pretrained(self.model_id)
@@ -27,5 +27,11 @@ class DefDetrModel(nn.module):
         # TODO: Add loading checkpoints
         return model
 
-    def forward(self, images: list[torch.Tensor], targets: list[dict[str, torch.Tensor]]):
-        pass
+    def forward(self, images: list[torch.Tensor], targets: list[dict[str, torch.Tensor]] | None = None):
+        inputs = self.processor(images=images, return_tensors='pt', device=self.device)
+
+        if targets is not None:
+            outputs = self.model(**inputs, labels=targets)
+        else:
+            outputs = self.model(**inputs)
+        return outputs
