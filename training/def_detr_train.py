9335126a652c35587c21893715671ead677a7a15
MikolajRusin <rusinmiko@gmail.com>
2025-11-07 01:50:16 +0100

    wip:

--- training/def_detr_train.py
+++ training/def_detr_train.py
@@ -1,73 +1,37 @@
 from pathlib import Path
 import sys
-import os
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
-from utils.data.load_dataloader import load_dataloader
+from utils.data_utils.load_dataloader import load_dataloader
+from logger.wandb_logger import WandbLogger
 from models.def_detr_model import DefDetrModel
+from training.transformer_trainer import TransformerTrainer
+from dotenv import load_dotenv, find_dotenv
+from omegaconf import OmegaConf
 import albumentations as A
 import argparse
-import torch
 
-# Path of the project
-PROJECT_PATH = Path(__file__).resolve().parent.parent
-# Main data dir
-DATA_PATH = PROJECT_PATH / 'data'
-# Paths to train, valid and test dir
-TRAIN_PATH = DATA_PATH / 'train'
-TRAIN_IMAGES_PATH = TRAIN_PATH / 'images'
-TRAIN_ANNOTATIONS = TRAIN_PATH / 'coco_annotations' / 'annotations.json'
-VALID_PATH = DATA_PATH / 'valid'
-VALID_IMAGES_PATH = VALID_PATH / 'images'
-VALID_ANNOTATIONS = VALID_PATH / 'coco_annotations' / 'annotations.json'
-TEST_PATH = DATA_PATH / 'test'
-TEST_IMAGES_PATH = TEST_PATH / 'images'
-TEST_ANNOTATIONS = TEST_PATH / 'coco_annotations' / 'annotations.json'
-
-MODEL_ID = 'SenseTime/deformable-detr'
-ID2LABEL = {
-    0: 'N/A',
-    1: 'Ball', 
-    2: 'Goalkeeper', 
-    3: 'Player', 
-    4: 'Referee'
-}
-
-def parse_arguments():
+def load_config():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--num_epochs', type=int, default=10, help='Number of training epochs. Defaults to 10.')
-    parser.add_argument('--batch_size', type=int, default=2, help='Batch size. Defaults to 2.')
-    parser.add_argument('--train_set_ratio', type=float, default=1.0, help='Ratio of the datasets (0.0 - 1.0] or an integer for number of images. Defaults to None.')
-    parser.add_argument('--valid_set_ratio', type=float, default=None, help='Ratio of the datasets (0.0 - 1.0] or an integer for number of images. Defaults to None.')
-    parser.add_argument('--shuffle', action='store_true', help='Shuffle dataset')
-    parser.add_argument('--normalize_boxes', action='store_true', help='Normalize boxes before passing them into the model')
-    parser.add_argument('--augmentation', action='store_true', help='Apply data augmentation')
-    parser.add_argument('--pin_memory', action='store_true', help='Apply pin memory')
-    parser.add_argument('--device', type=str, default='cpu', help='Device where the model will be stored')
-    # parser.add_argument('--backbone_lr', type=float, default=None, help="Learning rate for the backbone of the model. If None, the model will use the default learning rate ('lr') for all parameters. Defaults to None.")
-    # parser.add_argument('--learning_rate', type=float, default=0.0001, help="Learning rate for all model parameters, or for the non-backbone layers if 'backbone_lr' is set. Defaults to 0.0001.")
-    # parser.add_argument('--val_frequency', type=float, default=None, help='Frequency of model validation during epochs.')
-    # parser.add_argument('--save_checkpoints', type=bool, default=False, help='If True then the model save checkpoints.')
-    # parser.add_argument('--max_checkpoints', type=int, default=10, help='Maximum number of checkpoints contained in a folder.')
-    # parser.add_argument('--use_scheduler', type=bool, default=False, help='If True then use learning rate scheduler (CosineAnnealingLR).')
-    # parser.add_argument('--T_max', type=int, default=5, help='Number of epochs to reach minimum learning rate -> if use_scheduler=True.')
-    # parser.add_argument('--eta_min', type=float, default=1e-6, help='Minimum learning rate after T_max epochs -> if use_scheduler=True.')
-    # parser.add_argument('--log_results', type=bool, default=False, help='If True then log results in Weights & Biases (wandb)')
-    # parser.add_argument('--project_name', type=str, default='Model_fine_tuning', help='Name of the created project in Weights & Biases (wandb)')
+    parser.add_argument('--config_path', type=str, default='configs/config.yaml', help='Path to the configuration file')
     args = parser.parse_args()
 
-    # === Log used parameters ===
-    print('\n' + '=' * 80)
-    print('Training Configuration')
-    print('=' * 80)
-    for arg, value in vars(args).items():
-        print(f'{arg:20s}: {value}')
-    print('=' * 80 + '\n')
+    config = OmegaConf.load(args.config_path)
+    config = OmegaConf.to_container(config, resolve=True)
+    config = OmegaConf.create(config)
+    return config
 
-    return args
+CONFIG = load_config()
+
+# Paths of the project
+PROJECT_PATH      = Path(CONFIG.paths.project_root).resolve()
+TRAIN_IMAGES_PATH = Path(CONFIG.paths.train.images)
+TRAIN_ANNOTATIONS = Path(CONFIG.paths.train.annotations)
+VALID_IMAGES_PATH = Path(CONFIG.paths.valid.images) if getattr(CONFIG.paths.valid, 'images', None) else None
+VALID_ANNOTATIONS = Path(CONFIG.paths.valid.annotations) if getattr(CONFIG.paths.valid, 'annotations', None) else None 
 
 def main():
-    args = parse_arguments()
+    load_dotenv(find_dotenv())
 
     # Augmentation function
     transforms = A.Compose(
@@ -84,36 +48,63 @@ def main():
     # Train and Valid dataloaders
     new_categories = [
         {'id': k, 'name': v}
-        for k, v in ID2LABEL.items()
+        for k, v in CONFIG.model.id2label.items()
+        if v != 'N/A'
     ]
     train_dataloader = load_dataloader(
         root_dir=TRAIN_IMAGES_PATH,
         coco_path=TRAIN_ANNOTATIONS,
-        set_ratio=args.train_set_ratio,
+        set_ratio=CONFIG.training.train_set_ratio,
         custom_categories=new_categories,
-        batch_size=args.batch_size,
-        shuffle=args.shuffle,
-        transform_func=transforms if args.augmentation else None,
-        normalize_boxes=args.normalize_boxes,
-        pin_memory=args.pin_memory
+        batch_size=CONFIG.training.batch_size,
+        shuffle=CONFIG.training.shuffle,
+        transform_func=transforms if CONFIG.training.augmentation else None,
+        desire_bbox_format=CONFIG.training.desire_bbox_format,
+        pin_memory=CONFIG.training.pin_memory
     )
     valid_dataloader = None
-    if args['valid_set_ratio'] is not None:
-        valid_dataloader = load_dataloader(
-            root_dir=TRAIN_IMAGES_PATH,
-            coco_path=TRAIN_ANNOTATIONS,
-            set_ratio=args.valid_set_ratio,
-            custom_categories=new_categories,
-            batch_size=args.batch_size,
-            shuffle=args.shuffle,
-            transform_func=transforms if args.augmentation else None,
-            normalize_boxes=args.normalize_boxes,
-            pin_memory=args.pin_memory
-        )
+    if getattr(CONFIG.paths.valid, 'images', None):
+        if getattr(CONFIG.paths.valid, 'annotations', None):
+            valid_dataloader = load_dataloader(
+                root_dir=VALID_IMAGES_PATH,
+                coco_path=VALID_ANNOTATIONS,
+                set_ratio=CONFIG.training.valid_set_ratio,
+                custom_categories=new_categories,
+                batch_size=CONFIG.training.batch_size,
+                shuffle=CONFIG.training.shuffle,
+                transform_func=transforms if CONFIG.training.augmentation else None,
+                desire_bbox_format=CONFIG.training.desire_bbox_format,
+                pin_memory=CONFIG.training.pin_memory
+            )
+        else:
+            raise ValueError('If you want to use validation dataset to evaluate model, you must also specify the path to the annotations.')
 
+    # WandbLogger
+    wandb_logger = None
+    if CONFIG.training.log_metrics:
+        wandb_logger = WandbLogger(
+            project_name=CONFIG.wandb_logger.project_name, 
+            project_config=OmegaConf.to_container(CONFIG.wandb_logger.model_config, resolve=True)  # Convert to dict
+        )
+    
     # Model
-    device = args['device']
-    deformable_detr_model = DefDetrModel(model_id=MODEL_ID, id2label=ID2LABEL, device=device)
+    deformable_detr_model = DefDetrModel(model_id=CONFIG.model.model_id, id2label=CONFIG.model.id2label, device=CONFIG.training.device)
+
+    # Trainer
+    trainer = TransformerTrainer(
+        model=deformable_detr_model,
+        train_dataloader=train_dataloader,
+        valid_dataloader=valid_dataloader,
+        val_frequency=CONFIG.training.val_frequency,
+        wandb_logger=wandb_logger,
+        map_per_class=CONFIG.training.map_per_class,
+        n_epochs=CONFIG.training.num_epochs,
+        optimizer=CONFIG.optimizer.type,
+        optimizer_params=CONFIG.optimizer.params,
+        lr_scheduler=CONFIG.scheduler.type if getattr(CONFIG.scheduler, 'type', None) else None,
+        lr_scheduler_params=CONFIG.scheduler.params if getattr(CONFIG.scheduler, 'type', None) else None
+    )
+    trainer.train()
 
 if __name__ == '__main__':
     main()
--- training/transformer_trainer.py
+++ training/transformer_trainer.py
@@ -1,63 +1,220 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
+import torch.optim.lr_scheduler as lr_scheduler
+from utils.box_ops import denormalize_bboxes
+from tqdm import tqdm
 from dataclasses import dataclass
 from torch.utils.data import DataLoader
-from torch.optim import AdamW, lr_scheduler
+from logger.wandb_logger import WandbLogger
+from transformers.modeling_outputs import BaseModelOutput
+from evaluator import Evaluator
+import gc
 
 @dataclass
 class TransformerTrainer:
     model: nn.Module
     train_dataloader: DataLoader
     valid_dataloader: DataLoader | None = None
+    val_frequency: int | None = None
+    wandb_logger: WandbLogger | None = None
+    map_per_class: bool = False
     n_epochs: int = 5
-    optimizer_cls: torch.optim.Optimizer = AdamW
-    optimizer_additional_params: dict | None = None
-    lr_scheduler_cls: torch.optim.lr_scheduler.LRScheduler | None = None
+    optimizer: str = 'adamw'
+    optimizer_params: dict | None = None
+    lr_scheduler: str | None = None
     lr_scheduler_params: dict | None = None
-    lr: float = 1e-4
-    lr_backbone: float | None = None
-    weight_decay: float | None = None
 
     def __post_init__(self):
-        self.optimizer, self.lr_scheduler = self._configure_optimizers()
+        # Set optimizer and, if lr_scheduler is specified also lr_scheduler
+        self.optimizer, self.lr_scheduler = self._configure_optimizer_and_scheduler()
+        # Initialize evaluator to calculate metrics
+        self.evaluator = Evaluator(map_per_class=self.map_per_class)
+
+        # Lists for tracking loss
+        self.train_loss = []
+        self.valid_loss = []
 
     def train(self):
+        print(' Start Training '.center(90, '-'))
+        self._display_training_params()
+
         for cur_n_epoch in range(self.n_epochs):
-            pass
+            # Train
+            train_loss = self._train_one_epoch(cur_n_epoch)
+            self.train_loss.append(train_loss)   
+
+            # Evaluate
+            if self.valid_dataloader is not None:
+                valid_loss = self.evaluate_model(self.valid_dataloader)
+                self.valid_loss.append(valid_loss)
+
+            # Update learning rate if provided scheduler
+            if self.lr_scheduler is not None:
+                self.lr_scheduler.step()
+
+        print(' End Training '.center(90, '-'))
+
+    def _train_one_epoch(self, epoch):
+        running_loss = 0
+        running_box_loss = 0
+        running_total_samples = 0
+
+        # Iteration loop for training one epoch
+        for iteration, (batch_image, batch_target) in enumerate(tqdm(self.train_dataloader, total=len(self.train_dataloader), desc=f'Epoch {epoch}/{self.n_epochs}')):
+            torch.cuda.empty_cache()  # Clear the CUDA cache
+            self.model.train()        # Set the model to train mode
+
+            # Get the model's predictions for a batch, calculate losses and make the common steps
+            outputs = self.model(batch_image, batch_target)
+            loss = outputs.loss
+            self._common_steps(loss)
+
+            print('pred_boxespred_boxespred_boxespred_boxespred_boxes')
+            print(type(outputs))
+            print(outputs.pred_boxes)
+            print('\nlogitslogitslogitslogitslogitslogitslogits')
+            print(outputs.logits.argmax(dim=-1))
+            print('\nloss')
+            print(outputs.loss)
+            print('\nkeykeykeykeykeykeykey')
+            print(outputs.keys())
+            break
+            
+            # Add loss to the total loss for current epoch
+            running_loss += loss
+            running_total_samples += len(batch_image)
+            running_box_loss += box_loss
+
+            # Model validation with specified val_frequency
+            if self.val_frequency is not None and ((iteration + 1) % self.val_frequency) == 0:
+                freq_val_loss  =  self.evaluate_model(self.valid_dataloader)
+                avg_train_loss = running_loss / running_total_samples
+
+        return running_loss / len(self.train_dataloader)
+    
+    @torch.no_grad()
+    def evaluate_model(self, dataloader: DataLoader):
+        print(' Start Evaluating '.center(90, '-'))
+        self.model.eval()  # Set the model to valid mode
+
+        running_loss = 0
+        all_preds = []
+        all_targets = []
+        
+        # Iteration loop for model validation
+        for (batch_image, batch_target) in tqdm(dataloader, total=len(dataloader), desc='Evaluating model'):
+            outputs = self.model(batch_image, batch_target)
+            loss = outputs.loss
+            running_loss += loss
+
+            # if self.wandb_logger is not None:
+            preds    = self._postprocess_outputs_for_evaluator(outputs, batch_image)
+            targets  = self._postprocess_targets_for_evaluator(batch_target)
+
+        self.model.train()
+        return running_loss / len(dataloader)
+    
+    def _postprocess_outputs_for_evaluator(self, outputs: BaseModelOutput, batch_image: list[torch.Tensor]):
+        raw_logits = outputs.logits
+        raw_pred_boxes = outputs.pred_boxes
+
+        # Convert logits into scores and predicted class_ids
+        preds = raw_logits.softmax(-1)
+        scores, cls_ids = preds.max(-1)
+
+        # Filter classes where cls_id != 0 (Background)
+        keep_mask  = (cls_ids != 0)
+        scores     = scores[keep_mask]
+        cls_ids    = cls_ids[keep_mask]
+        pred_boxes = raw_pred_boxes[keep_mask]
+
+        # Add batch dimension for single image (if needed)
+        scores     = scores.unsqueeze(0) if scores.ndim == 1 else scores
+        cls_ids    = cls_ids.unsqueeze(0) if cls_ids.ndim == 1 else cls_ids
+        pred_boxes = pred_boxes.unsqueeze(0) if pred_boxes.ndim == 2 else pred_boxes
+
+        # Combine all data
+        preds_for_evaluator = [
+            {
+                'boxes': denormalize_bboxes(img_pred_boxes, image.shape[1], image.shape[2]),
+                'scores': img_scores.detach().cpu(),
+                'labels': img_cls_ids.detach().cpu()
+            }
+            for img_pred_boxes, img_scores, img_cls_ids, image 
+            in zip(pred_boxes, scores, cls_ids, batch_image)
+        ]
+        return preds_for_evaluator
+
+    def _postprocess_targets_for_evaluator(self, batch_target: list[dict[str, torch.Tensor]]):
+        targets_for_evaluator = [
+            {
+                'boxes': torch.stack([img_ann['bbox'] for img_ann in img_target['annotations']]),
+                'labels': torch.tensor([img_ann['category_id'] for img_ann in img_target['annotations']])
+            }
+            for img_target in batch_target
+        ]
+        return targets_for_evaluator
 
     def _common_steps(self, loss):
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
 
-    def _configure_optimizers(self) -> tuple[torch.optim.Optimizer, torch.optim.lr_scheduler.LRScheduler | None]:
-        # Param Groups
-        if self.backbone_lr is not None:
-            param_dicts = [
-                {'params': [p for n, p in self.model.named_parameters if 'backbone' in n and p.requires_grad], 'lr': self.backbone_lr},
-                {'params': [p for n, p in self.model.named_parameters if 'backbone' not in n and p.requires_grad]}
+    def _configure_optimizer_and_scheduler(self) -> tuple[torch.optim.Optimizer, torch.optim.lr_scheduler.LRScheduler | None]:
+        # Configure Optimizer
+        if getattr(self.optimizer_params, 'backbone_lr', None):
+            params_dict = [
+                {'params': [p for n, p in self.model.named_parameters() if 'backbone' in n and p.requires_grad], 'lr': self.optimizer_params['backbone_lr']},
+                {'params': [p for n, p in self.model.named_parameters() if 'backbone' not in n and p.requires_grad]}
             ]
         else:
-            param_dicts = [
+            params_dict = [
                 {'params': [p for _, p in self.model.named_parameters() if p.requires_grad]}
             ]
+        backbone_lr = self.optimizer_params.pop('backbone_lr', None)  # Temporary deletion of 'backbone_lr' before passing the parameters to optimizer
+        optimizer = self._get_optimizer_cls(self.optimizer)(params_dict, **self.optimizer_params)
+        self.optimizer_params['backbone_lr'] = backbone_lr  # Add previously deleted 'backbone_lr'
 
-        # Optimizer
-        optimizer_kwargs = {
-            'lr': self.lr, 
-            'weight_decay': self.weight_decay if self.weight_decay is not None else 0.01  # If self.weight_decay is None then use default values
+        # Configure Scheduler
+        scheduler = None
+        if self.lr_scheduler is not None:
+            scheduler = self._get_scheduler_cls(self.lr_scheduler)(optimizer, **self.lr_scheduler_params)
+
+        return optimizer, scheduler
+
+    def _get_optimizer_cls(self, optimizer_name: str) -> torch.optim.Optimizer:
+        optimizers_cls = {
+            'adamw': optim.AdamW,
+            'adam': optim.Adam,
+            'sgd': optim.SGD
         }
-        if self.optimizer_additional_params is not None:
-            self.optimizer_additional_params.pop('lr', None)
-            optimizer_kwargs.update(self.optimizer_additional_params)
-        optimizer = self.optimizer_cls(param_dicts, **optimizer_kwargs)
+        return optimizers_cls[optimizer_name.lower()]
 
-        # Learning Rate Scheduler
-        lr_scheduler = None
-        if self.lr_scheduler_cls is not None:
-            scheduler_kwargs = {'optimizer': optimizer}
-            if self.lr_scheduler_cls is not None:
-                scheduler_kwargs.update(self.lr_scheduler_cls)
-            lr_scheduler = self.lr_scheduler_cls(**scheduler_kwargs)
-        
-        return optimizer, lr_scheduler
+    def _get_scheduler_cls(self, scheduler_name: str) -> torch.optim.lr_scheduler.LRScheduler:
+        schedulers_cls = {
+            'cosine_annealing': lr_scheduler.CosineAnnealingLR,
+            'step': lr_scheduler.StepLR,
+            'onecycle': lr_scheduler.OneCycleLR
+        }
+        return schedulers_cls[scheduler_name.lower()]
+
+    def _display_training_params(self):
+        print(' Training Params '.center(90, '-'))
+        print(f'Training model: {self.model.__class__.__name__}')
+        print(f'The model has been loaded on {'cuda' if next(self.model.parameters()).is_cuda else 'cpu'}')
+        print(f'Number of images in train dataloader: {len(self.train_dataloader)}')
+        if self.valid_dataloader is not None:
+            print(f'Number of images in valid dataloader: {len(self.valid_dataloader)}')
+        print(f'Number of training epochs: {self.n_epochs}')
+        if self.val_frequency is not None:
+            print(f'Frequency of evaluating model: {self.val_frequency}')
+        print(f'Used optimizer: {self.optimizer.__class__.__name__}')
+        print(f"Optimizer's params: {self.optimizer_params}")
+        if self.lr_scheduler is not None:
+            print(f'Used learning rate scheduler: {self.lr_scheduler}')
+            print(f"Learning rate scheduler's params: {self.lr_scheduler_params}")
+        print(f"Log metrics: {'False' if self.wandb_logger is None else 'True'}")
+        if self.wandb_logger is not None:
+            print(f'Computer mAP per class: {self.map_per_class}')
+        print('\n')
