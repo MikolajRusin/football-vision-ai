0a75168646578ec80cec65e0a710800c546d456c
MikolajRusin <rusinmiko@gmail.com>
2025-11-07 01:46:00 +0100

    feat: added reset class embeddings to prevent high values in unused classes in the case of custom classes

--- models/def_detr_model.py
+++ models/def_detr_model.py
@@ -1,6 +1,8 @@
-from transformers import AutoImageProcessor, DeformableDetrConfig, DeformableDetrObjectDetection
+from transformers import DeformableDetrImageProcessor, DeformableDetrConfig, DeformableDetrForObjectDetection
+from transformers.modeling_outputs import BaseModelOutput
 import torch
 import torch.nn as nn
+from typing import Any
 
 class DefDetrModel(nn.Module):
     def __init__(self, model_id: str, id2label: dict[int, str] | None = None, device: str | None = None):
@@ -11,27 +13,41 @@ class DefDetrModel(nn.Module):
 
         # --- load config and model
         self.config = self._load_model_config()
-        self.model = self._load_model().to(device)
-        self.processor = self.AutoImageProcessor.from_pretrained(self.model_id, use_fast=True)
+        self.model = self._load_model().to(self.device)
+        self.processor = DeformableDetrImageProcessor.from_pretrained(self.model_id, use_fast=True)
 
     def _load_model_config(self):
         config = DeformableDetrConfig.from_pretrained(self.model_id)
         if self.id2label is not None:
             config.id2label   = self.id2label
             config.label2id   = {v: k for k, v in self.id2label.items()}
-            config.num_labels = len(self.label2id)
+            config.num_labels = len(self.id2label)
         return config
 
-    def _load_model(self):
-        model = DeformableDetrObjectDetection.from_pretrained(self.model_id, config=self.config)
-        # TODO: Add loading checkpoints
+    def _reset_class_embeddings(self, model):
+        for i in range(len(model.class_embed)):
+            nn.init.xavier_uniform(model.class_embed[i].weight)
+            if model.class_embed[i].bias is not None:
+                nn.init.constant_(model.class_embed[i].bias, 0)
+
+    def _load_model(self) -> DeformableDetrForObjectDetection:
+        model = DeformableDetrForObjectDetection.from_pretrained(self.model_id, config=self.config, ignore_mismatched_sizes=True)
+        if self.id2label is not None:
+            self._reset_class_embeddings(model)
         return model
 
-    def forward(self, images: list[torch.Tensor], targets: list[dict[str, torch.Tensor]] | None = None):
-        inputs = self.processor(images=images, return_tensors='pt', device=self.device)
-
+    # @torch.no_grad()
+    def forward(self, images: list[torch.Tensor], targets: list[dict[str, Any]] | None = None) -> BaseModelOutput:
         if targets is not None:
-            outputs = self.model(**inputs, labels=targets)
+            inputs = self.processor(images=images, annotations=targets, return_tensors='pt')
+            pixel_values = inputs['pixel_values'].to(self.device)
+            pixel_mask   = inputs['pixel_mask'].to(self.device)
+            labels       = [{k: v.to(self.device) for k, v in l.items()} for l in inputs['labels']]
+            outputs = self.model(pixel_values=pixel_values, pixel_mask=pixel_mask, labels=labels)
         else:
-            outputs = self.model(**inputs)
+            inputs = self.processor(images=images, return_tensors='pt')
+            pixel_values = inputs['pixel_values'].to(self.device)
+            pixel_mask   = inputs['pixel_mask'].to(self.device)
+            outputs = self.model(pixel_values=pixel_values, pixel_mask=pixel_mask)
+
         return outputs
